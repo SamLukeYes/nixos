c8f2fcac29891aad51fc27da5a8ee7175066ba45
SamLukeYes <samlukeyes123@gmail.com>
2022-08-11 21:44:53 +0800

    Make python packages available for xonsh

--- environment.nix
+++ environment.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
+let
+  my-python = pkgs.python3.withPackages (p: with p; [
+    ipykernel openpyxl pandas scipy
+  ]);
+in
+
 {
   environment = {
 
@@ -52,10 +58,6 @@
       adw-gtk3
       electron-netease-cloud-music
 
-      (python3.withPackages (p: with p; [
-        ipykernel openpyxl pandas scipy
-      ]))
-
       (vscode.fhsWithPackages (ps: with ps; [
         nodePackages.pyright
         texlive.combined.scheme-full
@@ -67,6 +69,7 @@
       GST_PLUGIN_PATH_1_0 = ["${pkgs.gst_all_1.gst-vaapi}/lib/gstreamer-1.0"];
       LIBVA_DRIVER_NAME = "iHD";
       MOZ_DBUS_REMOTE = "1";
+      PYTHONPATH = ["${my-python}/${my-python.sitePackages}"];
       QT_QPA_PLATFORM = "wayland;xcb";
     };
   };
