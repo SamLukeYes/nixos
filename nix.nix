8715feee1ed7297c37a87c3e2876866262cc1b70
SamLukeYes <samlukeyes123@gmail.com>
2022-08-20 01:03:22 +0800

    Add nix cache

--- nix.nix
+++ nix.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, options, ... }:
+{ config, pkgs, options, lib, ... }:
 
 let rp = import ./reverse-proxy.nix; in
 
@@ -11,13 +11,16 @@ let rp = import ./reverse-proxy.nix; in
     settings = {
       auto-optimise-store = true;
       keep-outputs = true;
-      substituters = [
+      substituters = lib.mkForce [
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "${rp}https://cache.nixos.org"
+        "${rp}https://nix-community.cachix.org"
         "${rp}https://linyinfeng.cachix.org"
         "${rp}https://shadowrz.cachix.org"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
         "shadowrz.cachix.org-1:I+6FCWMtdGmN8zYVncKdys/LVsLkCMWO3tfXbwQPTU0="
       ];
