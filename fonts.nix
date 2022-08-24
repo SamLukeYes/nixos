893a4e25d8d77bcc1336ca83120ccf1d917caf9e
SamLukeYes <samlukeyes123@gmail.com>
2022-08-24 09:45:47 +0800

    Use noto fonts as English fonts

--- fonts.nix
+++ fonts.nix
@@ -4,22 +4,23 @@
   fonts = {
     fonts = with pkgs; [
       inconsolata-nerdfont
+      noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
-      ubuntu_font_family
+      powerline-fonts
       win10-fonts
     ];
     fontconfig.defaultFonts = {
       serif = [
-        "DejaVu Serif"
+        "Noto Serif"
         "Noto Serif CJK SC"
       ];
       sansSerif = [
-        "Ubuntu"
+        "Noto Sans"
         "Noto Sans CJK SC"
       ];
       monospace = [
-        "DejaVu Sans Mono"
+        "Noto Sans Mono"
         "Noto Sans Mono CJK SC"
       ];
     };
