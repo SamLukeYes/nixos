3d141d243f440e0482327089b6dae352f1b7594e
SamLukeYes <samlukeyes123@gmail.com>
2022-09-07 13:13:43 +0800

    Remove mirrorz from pacman mirrorlist

--- environment.nix
+++ environment.nix
@@ -15,7 +15,6 @@ in
       "pacman.d/gnupg".source = "${pkgs.nur.repos.yes.archlinux.pacman-gnupg}/etc/pacman.d/gnupg";
       "pacman.d/mirrorlist".text = ''
         Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
-        Server = https://m.mirrorz.org/archlinux/$repo/os/$arch
         Server = https://mirror.sjtu.edu.cn/archlinux/$repo/os/$arch
       '';
     };
