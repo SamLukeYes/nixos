dd9ac65191539ca63d35bd66207655cd194761af
SamLukeYes <samlukeyes123@gmail.com>
2022-11-04 19:43:52 +0800

    Use garnix cache

--- common/nix.nix
+++ common/nix.nix
@@ -18,19 +18,15 @@ let rp = import ../rp.nix; in
       substituters = lib.mkForce [
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "${rp}https://cache.garnix.io"
         "${rp}https://cache.nixos.org"
-        "${rp}https://nix-community.cachix.org"
         "${rp}https://nixos-cn.cachix.org"
-        "${rp}https://linyinfeng.cachix.org"
         "${rp}https://rewine.cachix.org"
-        "${rp}https://xddxdd.cachix.org"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg="
-        "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
         "rewine.cachix.org-1:aOIg9PvwuSefg59gVXXxGIInHQI9fMpskdyya2xO+7I="
-        "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
       ];
     };
   };
