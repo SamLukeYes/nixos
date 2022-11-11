a112e7ed4a1e300d0efe9ae77323e160cf1bceb2
SamLukeYes <samlukeyes123@gmail.com>
2022-11-11 22:10:02 +0800

    Demote the priority of garnix cache

--- common/nix.nix
+++ common/nix.nix
@@ -18,15 +18,15 @@ let rp = import ../rp.nix; in
       substituters = lib.mkForce [
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
-        "${rp}https://cache.garnix.io"
         "${rp}https://cache.nixos.org"
         "${rp}https://nixos-cn.cachix.org"
         "${rp}https://rewine.cachix.org"
+        "${rp}https://cache.garnix.io"
       ];
       trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg="
         "rewine.cachix.org-1:aOIg9PvwuSefg59gVXXxGIInHQI9fMpskdyya2xO+7I="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
