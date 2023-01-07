610da23eb15f61e030f2aa6947af93aa06e1ddb0
SamLukeYes <samlukeyes123@gmail.com>
2023-01-07 19:40:44 +0800

    Fix up!!

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
             (final.substituteAll {
               src = final.fetchpatch {
                 url = "https://cdn.jsdelivr.net/gh/piousdeer/nixpkgs@pano/pkgs/desktops/gnome/extensions/extensionOverridesPatches/pano_at_elhan.io.patch";
-                sha256 = "1mlv9gha59g3jw03p4mf4fj5xiffchgivp6mn12k782w7mr2j588";
+                hash = "sha256-XYA2nPoNC0ft27NhiI7iAyOr/chnoXvyn3hCEDnzdfY=";
               };
               gda_path = "${final.libgda}/lib/girepository-1.0";
               gsound_path = "${final.gsound}/lib/girepository-1.0";
