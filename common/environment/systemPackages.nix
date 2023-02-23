b407454ca1bb9503b0612f4c53fe89645492550d
SamLukeYes <samlukeyes123@gmail.com>
2023-02-23 18:24:23 +0800

    Add nix-output-monitor to systemPackages

--- common/environment/systemPackages.nix
+++ common/environment/systemPackages.nix
@@ -34,6 +34,7 @@
     # CLI programs
     bat                           # frequently used in terminal
     dig                           # must be available without Internet connection
+    nix-output-monitor            # frequently used in nix operations
     onedrive                      # required by gnomeExtensions.one-drive-resurrect
     pdftk                         # required by Jasminum
     starship                      # configured in ~/.xonshrc
