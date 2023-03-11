0975596b9702631d6eb96306360d50317f1790ea
SamLukeYes <samlukeyes123@gmail.com>
2023-03-11 10:29:19 +0800

    Remove ventoy

--- common/environment/systemPackages.nix
+++ common/environment/systemPackages.nix
@@ -47,11 +47,6 @@
       icon = "${linyinfeng.yacd}/yacd-128.png";
     })
 
-    (ventoy-bin.override {
-      defaultGuiType = "gtk3";
-      withGtk3 = true;
-    })
-
     ((vscode.override {
       commandLineArgs = "--touch-events -n";
     }).fhsWithPackages (ps: with ps; [
