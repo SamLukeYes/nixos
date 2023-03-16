dfc6fca229aa972821dbcb727fafa98a8db36312
SamLukeYes <samlukeyes123@gmail.com>
2023-03-16 22:06:53 +0800

    Add NJU binary cache

--- common/nix.nix
+++ common/nix.nix
@@ -20,6 +20,7 @@ let rp = import ../rp.nix; in
       max-jobs = 3;   # https://github.com/NixOS/nixpkgs/issues/198668
       narinfo-cache-negative-ttl = 300;
       substituters = lib.mkForce [
+        "https://mirror.nju.edu.cn/nix-channels/store"
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://linyinfeng.cachix.org"
