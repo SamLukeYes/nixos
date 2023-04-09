acbc4327a9ab28e1d7544a631f9bc53061712f33
SamLukeYes <samlukeyes123@gmail.com>
2023-04-09 15:07:15 +0800

    Revert "Use libreoffice-fresh"
    This reverts commit d57457effa4bfc349a322de09d442bc05ab4c3ab.

--- flake.nix
+++ flake.nix
@@ -129,7 +129,6 @@
         '';
       });
       devtools = final.yes.archlinux.devtools;
-      libreoffice = final.libreoffice-fresh;
       linyinfeng = inputs.linyinfeng.packages.${system};
       # nil = inputs.nil.packages.${system}.nil;
       olex2 = inputs.olex2.packages.${system}.olex2-launcher-x11;
