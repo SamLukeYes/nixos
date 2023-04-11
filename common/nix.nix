abf0816d9e7cff768aa9ca48a64bb3b8b23df49e
SamLukeYes <samlukeyes123@gmail.com>
2023-04-11 17:42:53 +0800

    Revert "Add numtide cachix"
    This reverts commit 218d8bcb56c7aad2c6b37c204159bc969f6ee899.

--- common/nix.nix
+++ common/nix.nix
@@ -25,14 +25,12 @@ let rp = import ../rp.nix; in
         "https://mirror.iscas.ac.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://linyinfeng.cachix.org"
-        "https://numtide.cachix.org"
         "https://rewine.cachix.org"
         "${rp}https://cache.nixos.org"
         "${rp}https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "rewine.cachix.org-1:aOIg9PvwuSefg59gVXXxGIInHQI9fMpskdyya2xO+7I="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
