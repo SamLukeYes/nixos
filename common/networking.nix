1f3b1b9dac0d9bde61b0683d2fbae26d61fd0933
SamLukeYes <samlukeyes123@gmail.com>
2023-11-23 14:58:38 +0800

    Open port for ss-ws

--- common/networking.nix
+++ common/networking.nix
@@ -1,7 +1,10 @@
 {
   networking = {
     firewall = rec {
-      allowedTCPPorts = [ 23332 ];    # for lx-music sync
+      allowedTCPPorts = [
+        8787    # make shadowsocks-ws available to other devices
+        23332   # for lx-music sync
+      ];
       allowedUDPPorts = allowedTCPPorts;
     };
     networkmanager.enable = true;
