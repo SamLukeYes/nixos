ab6349bd20a3139b8174a9e5e41dc05779997d53
SamLukeYes <samlukeyes123@gmail.com>
2023-11-24 17:14:01 +0800

    Use onedrivegui

--- optional/apps/onedrive.nix
+++ optional/apps/onedrive.nix
@@ -1,14 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
-    onedrive
-  ] ++ lib.optional 
-    config.services.xserver.desktopManager.gnome.enable
-    gnomeExtensions.one-drive-resurrect;
-  
-  systemd = {
-    packages = [ pkgs.onedrive ];
-    user.services.onedrive.wantedBy = [ "default.target" ];
-  };
+    onedrivegui
+    (makeAutostartItem {
+      name = "OneDriveGUI";
+      package = onedrivegui;
+    })
+  ];
 }
