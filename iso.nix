ac396b667e8bb9fb4b5e771e1ebc830ab21978ad
SamLukeYes <samlukeyes123@gmail.com>
2024-03-06 00:31:11 +0800

    Add arch-install-scripts to iso

--- iso.nix
+++ iso.nix
@@ -1,9 +1,13 @@
-{ config, lib, modulesPath, ... }:
+{ config, lib, modulesPath, pkgs, ... }:
 
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-graphical-gnome.nix"
     ./common
+  ];
+
+  environment.systemPackages = with pkgs; [
+    arch-install-scripts
   ];
 
   isoImage.squashfsCompression = "zstd";
