0f3e2fff919a7382111f5b46f73f417065a08287
SamLukeYes <samlukeyes123@gmail.com>
2024-03-09 02:12:37 +0800

    Patch xonsh

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
+      ./patches/xonsh-avoid-double-wrap.patch
     ];
 
   in flake-utils-plus.lib.mkFlake rec {
