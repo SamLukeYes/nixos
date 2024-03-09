f6b28187c11f4e225e7fad507ee5cafee4823885
SamLukeYes <samlukeyes123@gmail.com>
2024-03-09 20:49:30 +0800

    Fix BROWSER path

--- common/environment/default.nix
+++ common/environment/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [ ./systemPackages.nix ];
@@ -7,7 +7,7 @@
 
     sessionVariables = {
       BASH_COMPLETIONS = ["${pkgs.bash-completion}/share/bash-completion/bash_completion"];
-      BROWSER = "${config.programs.firefox.package}/bin/firefox";
+      BROWSER = lib.getExe config.programs.firefox.package;
       LIBVA_DRIVER_NAME = "iHD";
       MOZ_DBUS_REMOTE = "1";
       MOZ_ENABLE_WAYLAND = "1";
