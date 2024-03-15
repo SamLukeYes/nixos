cded4bbd4b1583f9966b1cc8a8e4f714db108ef1
SamLukeYes <samlukeyes123@gmail.com>
2024-03-15 11:10:20 +0800

    Open 8080 port for testing

--- common/networking.nix
+++ common/networking.nix
@@ -2,6 +2,7 @@
   networking = {
     firewall = rec {
       allowedTCPPorts = [
+        8080    # for testing
         8787    # make shadowsocks-ws available to other devices
         23332   # for lx-music sync
       ];
