c0b75994c0ee99e7b248ba368499f12227762b46
SamLukeYes <samlukeyes123@gmail.com>
2024-03-18 00:16:05 +0800

    Revert "Use patched flatpak instead of bindfs workaround"
    This reverts commit d2a9090d1042fd03db75b06a7e4766eec983267d.

--- common/services/flatpak.nix
+++ common/services/flatpak.nix
@@ -1,22 +1,22 @@
-# { pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services.flatpak.enable = true;
 
   # https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279
-  # fonts.fontDir.enable = true;
-  # system.fsPackages = [ pkgs.bindfs ];
-  # fileSystems = let
-  #   mkRoSymBind = path: {
-  #     device = path;
-  #     fsType = "fuse.bindfs";
-  #     noCheck = true;
-  #     options = [ "ro" "resolve-symlinks" "x-gvfs-hide" "x-systemd.automount" ];
-  #   };
-  # in {
-  #   # Create an FHS mount to support flatpak host icons/fonts
-  #   # Should remove after https://github.com/flatpak/flatpak/issues/4692 is fixed
-  #   "/usr/share/icons" = mkRoSymBind "/run/current-system/sw/share/icons";
-  #   "/usr/share/fonts" = mkRoSymBind "/run/current-system/sw/share/X11/fonts";
-  # };
+  fonts.fontDir.enable = true;
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      noCheck = true;
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" "x-systemd.automount" ];
+    };
+  in {
+    # Create an FHS mount to support flatpak host icons/fonts
+    # Should remove after https://github.com/flatpak/flatpak/issues/4692 is fixed
+    "/usr/share/icons" = mkRoSymBind "/run/current-system/sw/share/icons";
+    "/usr/share/fonts" = mkRoSymBind "/run/current-system/sw/share/X11/fonts";
+  };
 }
