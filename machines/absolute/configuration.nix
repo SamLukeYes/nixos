b70cbc4c3da8d2b9556afff71d6d120c26b991e1
SamLukeYes <samlukeyes123@gmail.com>
2024-03-21 13:00:47 +0800

    Adjust thinkfan levels

--- machines/absolute/configuration.nix
+++ machines/absolute/configuration.nix
@@ -13,6 +13,10 @@
   services.thinkfan = {
     enable = true;
     smartSupport = true;
+    levels = [
+      ["level auto" 0 60]
+      [7 59 32767]
+    ];
   };
 
   system.stateVersion = "22.11";
