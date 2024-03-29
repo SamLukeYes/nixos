19815ad08a1c482b9ae9d6114662de8bab5c4f4f
SamLukeYes <samlukeyes123@gmail.com>
2024-03-29 14:46:17 +0800

    Open RDP port

--- common/networking.nix
+++ common/networking.nix
@@ -3,6 +3,7 @@
     firewall = rec {
       allowedTCPPorts = [
         2425    # for iptux
+        3389    # for rdp
         8787    # make shadowsocks-ws available to other devices
         23332   # for lx-music sync
       ];
