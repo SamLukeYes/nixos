04c8112bd08252b657a29044b9364ced1b6009d3
SamLukeYes <samlukeyes123@gmail.com>
2024-04-17 20:59:12 +0800

    Always use ibus instead of fcitx5

--- common/i18n.nix
+++ common/i18n.nix
@@ -1,24 +1,14 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   i18n = {
     # defaultLocale = "zh_CN.UTF-8";
-    inputMethod = if config.services.xserver.desktopManager.gnome.enable
-      then {
-        enabled = "ibus";
-        ibus.engines = with pkgs.ibus-engines; [
-          libpinyin
-        ];
-      }
-      else {
-        enabled = "fcitx5";
-        fcitx5 = {
-          addons = with pkgs; [
-            fcitx5-chinese-addons
-          ];
-          waylandFrontend = true;
-        };
-      };
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [
+        libpinyin
+      ];
+    };
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
       "zh_CN.UTF-8/UTF-8"
