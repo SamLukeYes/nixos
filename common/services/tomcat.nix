f9da6069fe3c5166b7541d184b9e6c24be1e438b
SamLukeYes <samlukeyes123@gmail.com>
2024-04-18 09:16:05 +0800

    Adjust tomcat ports

--- common/services/tomcat.nix
+++ common/services/tomcat.nix
@@ -1,10 +1,22 @@
+{ config, lib, ... }:
+
 {
+  # Open more ports for testing
   networking.firewall = rec {
-    allowedTCPPorts = [ 8080 8081 ];
-    allowedUDPPorts = allowedTCPPorts;
+    allowedTCPPortRanges = [{
+      from = 8080;
+      to = 8083;
+    }];
+    allowedUDPPortRanges = allowedTCPPortRanges;
   };
+
   services.tomcat = {
     enable = true;
     webapps = [];
   };
+
+  # Use port 8082 instead of 8080
+  systemd.services.tomcat.preStart = lib.mkAfter ''
+    sed -i "s/8080/8082/" ${config.services.tomcat.baseDir}/conf/server.xml
+  '';
 }
