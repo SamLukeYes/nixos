d62c36aad0fe4e455c352ac239dc1ae76e3509ee
SamLukeYes <samlukeyes123@gmail.com>
2024-05-04 21:45:05 +0800

    No longer open ss-ws port by default

--- common/networking.nix
+++ common/networking.nix
@@ -4,7 +4,6 @@
       allowedTCPPorts = [
         2425    # for iptux
         3389    # for rdp
-        8787    # make shadowsocks-ws available to other devices
         23332   # for lx-music sync
       ];
       allowedUDPPorts = allowedTCPPorts;
