a62ae1f7c30387bcf6e971b6acc43f30e9dcc839
SamLukeYes <samlukeyes123@gmail.com>
2024-05-23 08:47:54 +0800

    Add libGL to vscode-fhs

--- optional/apps/default.nix
+++ optional/apps/default.nix
@@ -11,6 +11,7 @@
     ((vscode.override {
       commandLineArgs = "--touch-events --enable-wayland-ime --disable-gpu-shader-disk-cache -n";
     }).fhsWithPackages (ps: with ps; [
+      libGL                             # required by conda env
       nil                               # for nix IDE
       pacman                            # add a dummy makepkg.conf to FHS
       python3Packages.python-lsp-server # for xonsh IDE
