0937e3bfe1e24c3a1e78679a66530024159be70a
SamLukeYes <samlukeyes123@gmail.com>
2024-06-03 14:46:22 +0800

    Add ffmpeg and yt-dlp-light

--- common/environment/systemPackages.nix
+++ common/environment/systemPackages.nix
@@ -14,6 +14,7 @@
     bat                           # frequently used to view text in terminal
     conda                         # manage python env
     dig                           # must be available without Internet connection
+    ffmpeg                        # video editing
     file                          # frequently used to view executable type
     lsof                          # view port usage
     nix-tree                      # frequently used to inspect space usage
@@ -21,6 +22,7 @@
     sshfs                         # frequently used for file sharing
     starship                      # configured without nix
     subversion                    # version control system
+    yt-dlp-light                  # frequently used to download videos
 
     # other out-of-tree packages
     yes.iptux
