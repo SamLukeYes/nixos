a38848d9fe5b2d089798dbaa0dd1819bc5bce18c
SamLukeYes <samlukeyes123@gmail.com>
2024-06-15 00:56:58 +0800

    Use Noto Serif instead of Source Hans Serif

--- common/fonts.nix
+++ common/fonts.nix
@@ -4,13 +4,16 @@
   fonts = {
     packages = with pkgs; [
       noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       powerline-fonts
-      source-han-serif
     ];
     fontconfig.defaultFonts = {
       sansSerif = [
         "Cantarell"
         "Noto Sans CJK SC"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
       ];
       monospace = [
         "Noto Sans Mono CJK SC"
