1b1848ba5c4854c905a6a86b78552755a468722e
SamLukeYes <samlukeyes123@gmail.com>
2024-08-04 19:29:50 +0800

    Allow non-root kubectl

--- optional/containers/default.nix
+++ optional/containers/default.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 
 {
-  services.k3s.enable = true;
+  services.k3s = {
+    enable = true;
+    extraFlags = [ "--write-kubeconfig-mode 0644" ];
+  };
+
   systemd.services.k3s.environment = config.networking.proxy.envVars;
 }
