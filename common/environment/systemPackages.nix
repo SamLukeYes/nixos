ef8a83b627f86d578c7e25d50dbe8030a29879f2
SamLukeYes <samlukeyes123@gmail.com>
2024-09-19 00:28:50 +0800

    Use SQLTools

--- common/environment/systemPackages.nix
+++ common/environment/systemPackages.nix
@@ -12,9 +12,9 @@
     }).fhsWithPackages (ps: with ps; [
       libGL                             # required by conda env
       nil                               # for nix IDE
+      nodejs                            # for SQLTools SQLite
       pacman                            # add a dummy makepkg.conf to FHS
       python3Packages.python-lsp-server # for xonsh IDE
-      sqlite                            # allow connection to sqlite database
       xclip                             # for Office Viewer Markdown Editor
     ]))
 
