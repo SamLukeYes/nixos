d3c0b923104f9500661cf8a3a186deb1291d919b
SamLukeYes <samlukeyes123@gmail.com>
2024-11-25 00:09:28 +0800

    Make earlyoom more aggressive on swap usage

--- common/services/earlyoom.nix
+++ common/services/earlyoom.nix
@@ -1,7 +1,11 @@
+{ ... }:
+
 {
   services.earlyoom = {
     enable = true;
     enableNotifications = true;
+    freeSwapThreshold = 50;
+    freeSwapKillThreshold = 10;
   };
 
   systemd.oomd.enable = false;
