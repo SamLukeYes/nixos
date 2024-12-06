db4a688c4ea996993e93f0e91a15b1db48a43504
SamLukeYes <samlukeyes123@gmail.com>
2024-12-06 22:47:10 +0800

    Drop yt-dlp patch

--- flake.nix
+++ flake.nix
@@ -137,9 +137,6 @@
       rewine = import inputs.rewine { pkgs = final; };
       xontribs = import inputs.xontribs { pkgs = final; };
       yes = import inputs.yes { pkgs = final; };
-      yt-dlp = prev.yt-dlp.overrideAttrs (old: {
-        patches = [ ./patches/yt-dlp-11667.patch ];
-      });
     };
   };
 }
