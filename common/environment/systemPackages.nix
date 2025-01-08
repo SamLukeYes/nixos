04a461e275757d5fa7bc5e4054fca2e5380a8dc8
SamLukeYes <samlukeyes123@gmail.com>
2025-01-09 01:08:35 +0800

    Move vscode-fhs cli packages to systemPackages

--- common/environment/systemPackages.nix
+++ common/environment/systemPackages.nix
@@ -12,11 +12,6 @@
       commandLineArgs = "--touch-events --enable-wayland-ime --disable-gpu-shader-disk-cache -n";
     }).fhsWithPackages (ps: with ps; [
       libGL                             # required by conda env
-      nil                               # for nix IDE
-      nodejs                            # for SQLTools SQLite
-      pacman                            # add a dummy makepkg.conf to FHS
-      python3Packages.python-lsp-server # for xonsh IDE
-      xclip                             # for Office Viewer Markdown Editor
     ]
     # https://github.com/NixOS/nixpkgs/issues/356340
     ++ lib.optional config.services.xserver.desktopManager.gnome.enable gnome-shell
@@ -29,12 +24,16 @@
     ffmpeg-headless               # video editing
     file                          # frequently used to view executable type
     lsof                          # view port usage
+    nil                           # for VSCode nix IDE
     nix-tree                      # frequently used to inspect space usage
+    nodejs                        # for VSCode SQLTools SQLite
     paru                          # frequently used to query AUR packages
+    python3Packages.python-lsp-server  # for VSCode xonsh IDE
     sqlite                        # to fix nix database in case of corruption
     starship                      # configured without nix
     subversion                    # version control system
     waypipe                       # wayland support for containerized apps
+    wl-clipboard-x11              # for VSCode Office Viewer Markdown Editor
     yt-dlp-light                  # frequently used to download videos
 
     # out-of-tree packages
