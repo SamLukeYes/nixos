e8059c66482fef0cf7ace7fea1d67d56094aca7d
SamLukeYes <samlukeyes123@gmail.com>
2025-02-03 00:16:47 +0800

    Actually fix virtualbox launch

--- common/boot.nix
+++ common/boot.nix
@@ -6,7 +6,10 @@
       v4l2loopback      
     ];
     initrd.systemd.enable = true;
-    kernelParams = lib.optional (!config.zramSwap.enable) "zswap.enabled=1";
+    kernelParams =
+      lib.optional (!config.zramSwap.enable) "zswap.enabled=1"
+      # https://github.com/NixOS/nixpkgs/issues/363887
+      ++ lib.optional (with config.virtualisation.virtualbox.host; enable && !enableKvm) "kvm.enable_virt_at_load=0";
     kernel.sysctl = {
       "dev.i915.perf_stream_paranoid" = 0;
     };
