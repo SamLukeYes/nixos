bab5f770047dd8830d03d03dc058537421b33b1c
SamLukeYes <samlukeyes123@gmail.com>
2025-02-19 23:15:25 +0800

    Clean up xonsh config

--- common/programs/xonsh.nix
+++ common/programs/xonsh.nix
@@ -10,10 +10,10 @@
       if __xonsh__.env.get("XDG_SESSION_TYPE") == "wayland":
           $GTK_IM_MODULE = ""
           $QT_IM_MODULE = ""
-          $MOZ_ENABLE_WAYLAND = "1";
-          $NIXOS_OZONE_WL = "1";
-          $QT_QPA_PLATFORM = "wayland;xcb";
-          $SDL_VIDEODRIVER = "wayland";
+          $MOZ_ENABLE_WAYLAND = "1"
+          $NIXOS_OZONE_WL = "1"
+          $QT_QPA_PLATFORM = "wayland;xcb"
+          $SDL_VIDEODRIVER = "wayland"
 
       # TODO: this shouldn't be necessary
       $SHELL_TYPE = "best"
