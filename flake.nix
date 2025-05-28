9666b8d048c85a9977362ad19b6000504b131766
SamLukeYes <samlukeyes123@gmail.com>
2025-05-28 22:18:10 +0800

    Drop applied patch

--- flake.nix
+++ flake.nix
@@ -48,7 +48,6 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
-      ./patches/starship-xonsh.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
