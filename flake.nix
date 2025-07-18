19c27e136019c769c77937b3041715566c86d2e4
SamLukeYes <samlukeyes123@gmail.com>
2025-07-19 07:24:51 +0800

    Drop applied patch

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
-      ./patches/fix-clisp.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
