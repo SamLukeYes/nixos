443c350de909274207688ddb0957f30f4a07fba0
SamLukeYes <samlukeyes123@gmail.com>
2025-07-29 23:15:42 +0800

    Update waydroid config

--- common/virtualisation/waydroid.nix
+++ common/virtualisation/waydroid.nix
@@ -7,13 +7,19 @@
 
   services.dbus.packages = [ pkgs.waydroid-helper ];
 
-  systemd.packages = [ pkgs.waydroid-helper ];
+  systemd = {
+    packages = [ pkgs.waydroid-helper ];
+    tmpfiles.rules = [
+      "f+ /var/lib/waydroid/waydroid.log 0666 root root"
+    ];
+  };
 
   preservation.preserveAt."/persistent".directories = [
     "/var/lib/waydroid"
   ];
 
   users.persistence.directories = [
+    ".config/systemd/user/waydroid-monitor.service.d/"
     ".local/share/waydroid"
   ];
 }
