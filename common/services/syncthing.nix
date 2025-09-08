f83a707ed70112f99fe8403f31ead101179e5131
SamLukeYes <samlukeyes123@gmail.com>
2025-09-08 22:22:22 +0800

    Use syncthingtray instead of gnome extension

--- common/services/syncthing.nix
+++ common/services/syncthing.nix
@@ -1,6 +1,8 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
-{
+let
+  syncthingtrayPkg = pkgs.syncthingtray-minimal;
+in {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
@@ -9,12 +11,24 @@
 
   systemd.user.services.syncthing.wantedBy = [ "graphical-session.target" ];
 
-  environment.systemPackages = [
-    pkgs.syncthing
-  ] ++ lib.optional config.services.desktopManager.gnome.enable
-    pkgs.gnomeExtensions.syncthing-indicator;
-
-  users.persistence.directories = [
-    ".config/syncthing"
+  environment.systemPackages = with pkgs; [
+    syncthing
+    syncthingtrayPkg
+    (makeAutostartItem {
+      name = "syncthingtray";
+      package = syncthingtrayPkg;
+    })
   ];
+
+  users.persistence = {
+    directories = [
+      ".config/syncthing"
+    ];
+    files = [
+      {
+        file = ".config/syncthingtray.ini";
+        how = "symlink";
+      }
+    ];
+  };
 }
