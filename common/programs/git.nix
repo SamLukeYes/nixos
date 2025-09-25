a8e35e17597586fd92b959a2e08147cfafcc3644
SamLukeYes <samlukeyes123@gmail.com>
2025-09-25 21:41:04 +0800

    Persist gh extensions

--- common/programs/git.nix
+++ common/programs/git.nix
@@ -5,11 +5,13 @@
 
   environment.systemPackages = with pkgs; [
     gh
+    jq  # required by gh-gonest
   ];
 
   users.persistence = {
     directories = [
       ".config/gh"
+      ".local/share/gh"
     ];
     files = [
       ".gitconfig"
