b01f218cc7b593906978d2b0e98489e6abda8d8c
SamLukeYes <samlukeyes123@gmail.com>
2025-10-02 03:26:45 +0800

    Update arch-install-scripts

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
+      ./patches/arch-install-scripts-update.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
