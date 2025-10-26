40ced6932c3191083921f07ca8dfcb8ecc8dc9d4
SamLukeYes <samlukeyes123@gmail.com>
2025-10-26 18:16:07 +0800

    Drop applied patch

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
     channel-patches = [
       # Add nixpkgs patches here
       ./patches/arch-install-scripts-update.patch
-      ./patches/fix-python-packages.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
