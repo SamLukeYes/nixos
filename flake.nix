6f24f770f70424df23db185da83ae46d16e8deb1
SamLukeYes <samlukeyes123@gmail.com>
2025-11-12 23:58:10 +0800

    Remove applied patch

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
-      ./patches/arch-install-scripts-update.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
