74e86e3f09946bd2465c509a1ebbc64cfbfd256c
SamLukeYes <samlukeyes123@gmail.com>
2025-11-30 18:49:45 +0800

    Update xonsh

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
+      ./update-xonsh.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
