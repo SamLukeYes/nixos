964d9d2307c7c38e0d708dc889683fba1f593738
SamLukeYes <samlukeyes123@gmail.com>
2025-12-08 11:11:44 +0800

    Drop patch

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
-      ./update-xonsh.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
