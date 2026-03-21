e7d18e090769abdf1daa1396c6b4ea79f6021d63
SamLukeYes <samlukeyes123@gmail.com>
2026-03-21 16:07:21 +0800

    Add xonsh patches

--- flake.nix
+++ flake.nix
@@ -42,6 +42,8 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
+      ./patches/xonsh-tests.patch
+      ./patches/xonsh-update.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
