3747113fe0ea4652f253ef0912de5c093999ec38
SamLukeYes <samlukeyes123@gmail.com>
2026-06-14 19:24:34 +0800

    disable legacy initrd for iso

--- iso.nix
+++ iso.nix
@@ -7,8 +7,6 @@
     ./optional/desktop/gnome.nix
   ];
 
-  boot.initrd.systemd.enable = lib.mkForce false;
-
   environment.systemPackages = [
     config.services.mihomo.package
     pkgs.arch-install-scripts
