d11a7653a3ef8a3d04c3948e3f553174c1f17bd7
SamLukeYes <samlukeyes123@gmail.com>
2026-06-19 22:07:22 +0800

    Add my cachix

--- common/nix.nix
+++ common/nix.nix
@@ -28,10 +28,12 @@
       ];
       extra-substituters = [
         "https://nix-community.cachix.org"
+        "https://samlukeyes123.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "samlukeyes123.cachix.org-1:AsNJmUZGo73EYpBhz2w/XuysdNXxvf6WcZO7qWVh2TQ="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
