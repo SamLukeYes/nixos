e4f220861f676ac19f0bba9c26808ddf5450c5ba
SamLukeYes <samlukeyes123@gmail.com>
2026-06-19 22:12:12 +0800

    Update pacman

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@
     system = "x86_64-linux";
     channel-patches = [
       # Add nixpkgs patches here
+      ./patches/pacman-update.patch
     ];
     nixpkgs-patched =
       flake-utils-plus.lib.patchChannel system nixpkgs channel-patches;
