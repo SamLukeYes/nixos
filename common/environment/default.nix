085fcb31ff8b6c269f28e74b55773c6fd238e748
SamLukeYes <samlukeyes123@gmail.com>
2026-07-25 22:47:01 +0800

    Move firefox-related variables to firefox.nix

--- common/environment/default.nix
+++ common/environment/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
   imports = [
@@ -10,10 +10,6 @@
     defaultPackages = [];
 
     variables = {
-      BROWSER = lib.getExe config.programs.firefox.package;
-      LIBVA_DRIVER_NAME = "iHD";
-      MOZ_DBUS_REMOTE = "1";
-      MOZ_USE_XINPUT2 = "1";
       NIXOS_OZONE_WL = "1";
       TZ = "/etc/localtime";
     };
--- common/programs/firefox.nix
+++ common/programs/firefox.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.firefox = {
@@ -10,6 +10,13 @@
       DisablePocket = true;
       DisableTelemetry = true;
     };
+  };
+
+  environment.variables = {
+    BROWSER = lib.getExe config.programs.firefox.package;
+    LIBVA_DRIVER_NAME = "iHD";
+    MOZ_DBUS_REMOTE = "1";
+    MOZ_USE_XINPUT2 = "1";
   };
 
   users.persistence.directories = [
