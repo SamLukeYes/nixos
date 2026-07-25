95d5f18094c6f9d7c7791666a0d3c41f776560d9
SamLukeYes <samlukeyes123@gmail.com>
2026-07-25 23:10:18 +0800

    Separate some programs to misc-cli.nix

--- common/programs/default.nix
+++ common/programs/default.nix
@@ -14,6 +14,7 @@
     ./git.nix
     ./gnupg.nix
     ./libreoffice.nix
+    ./misc-cli.nix
     ./nodejs.nix
     ./pacman.nix
     ./pixi.nix
@@ -21,42 +22,4 @@
     ./xonsh.nix
     ./xournalpp.nix
   ];
-
-  programs = {
-    bat.enable = true;
-
-    command-not-found.enable = false;
-
-    # requires nix-index-database
-    nix-index-database.comma.enable = true;
-
-    starship = {
-      enable = true;
-      settings = {
-        battery.display = [
-          {
-            style = "bold red";
-            threshold = 40;
-          }
-          {
-            style = "bold yellow";
-            threshold = 50;
-          }
-          {
-            style = "bold green";
-            threshold = 100;
-          }
-        ];
-        line_break.disabled = true;
-        package.disabled = true;
-        shell.disabled = false;
-        shlvl.disabled = false;
-        status.disabled = false;
-        time = {
-          disabled = false;
-          format = "🕙$time($style) ";
-        };
-      };
-    };
-  };
 }
---
+++ common/programs/misc-cli.nix
@@ -0,0 +1,41 @@
+{ ... }:
+
+{
+  programs = {
+    bat.enable = true;
+
+    command-not-found.enable = false;
+
+    # requires nix-index-database
+    nix-index-database.comma.enable = true;
+
+    starship = {
+      enable = true;
+      settings = {
+        battery.display = [
+          {
+            style = "bold red";
+            threshold = 40;
+          }
+          {
+            style = "bold yellow";
+            threshold = 50;
+          }
+          {
+            style = "bold green";
+            threshold = 100;
+          }
+        ];
+        line_break.disabled = true;
+        package.disabled = true;
+        shell.disabled = false;
+        shlvl.disabled = false;
+        status.disabled = false;
+        time = {
+          disabled = false;
+          format = "🕙$time($style) ";
+        };
+      };
+    };
+  };
+}
